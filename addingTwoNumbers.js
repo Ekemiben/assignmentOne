0a49f8fc580ddbe22e7978520e62f8e29e3a275d
Ekemini Sunday Ben <86943439+Ekemiben@users.noreply.github.com>
2022-07-15 09:35:30 +0100

    Update addingTwoNumbers.js

--- addingTwoNumbers.js
+++ addingTwoNumbers.js
@@ -1,9 +1,9 @@
-let x = twoNums(([2,7,11, 15]), ([ 9]))
 function twoNums(nums, target){
     let emptiArr = []
     let indexMap = new Map();
     for (let i = 0; i <nums.length; i++){
         let difference = target - nums[i]
+        console.log(difference)
         if(indexMap.has(difference)){
             emptiArr[0] = i;
             emptiArr[1] = indexMap.get(difference);
@@ -16,6 +16,7 @@ function twoNums(nums, target){
     return emptiArr;
 }
 
+let x = twoNums(([2,7,11, 15]), ([ 9]))
 console.log((x))
 
 
